9c03f37cd1f884dc1ccd076cfcaf58d0b3898681
Mike <z.mike0411@gmail.com>
2015-11-06 21:54:56 +0800

    stockist register function done

--- app/src/main/java/com/example/lenovo/osc/RegisterActivity.java
+++ app/src/main/java/com/example/lenovo/osc/RegisterActivity.java
@@ -1,12 +1,28 @@
 package com.example.lenovo.osc;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
 
 
 public class RegisterActivity extends ActionBarActivity {
+
+    protected EditText tfStockistID;
+    protected EditText tfName;
+    protected EditText tfPassword;
+    protected EditText tfIC;
+    protected EditText tfTel;
+    protected EditText tfEmail;
+    protected EditText tfAddress;
+    protected ParseObject regUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +50,55 @@ public class RegisterActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void register(View view) {
+
+        tfStockistID = (EditText) findViewById(R.id.tfRegStockistID);
+        tfName = (EditText) findViewById(R.id.tfRegName);
+        tfPassword = (EditText) findViewById(R.id.tfRegPassword);
+        tfIC = (EditText) findViewById(R.id.tfRegIC);
+        tfTel = (EditText) findViewById(R.id.tfRegTel);
+        tfEmail = (EditText) findViewById(R.id.tfRegEmail);
+        tfAddress = (EditText) findViewById(R.id.tfRegAddress);
+
+        String stockistID = tfStockistID.getText().toString();
+        String name = tfName.getText().toString();
+        String password = tfPassword.getText().toString();
+        String ic = tfIC.getText().toString();
+        String tel = tfTel.getText().toString();
+        String email = tfEmail.getText().toString();
+        String address = tfAddress.getText().toString();
+
+        if (TextUtils.isEmpty(stockistID)) {
+            tfStockistID.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(name)) {
+            tfName.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(password)) {
+            tfPassword.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(ic)) {
+            tfIC.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(tel)) {
+            tfTel.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(email)) {
+            tfEmail.setError("This field cannot be empty.");
+        } else if (TextUtils.isEmpty(address)) {
+            tfAddress.setError("This field cannot be empty.");
+        } else {
+
+            ParseObject regUser = new ParseObject("Stockist");
+            regUser.put("Address", address);
+            regUser.put("Email", email);
+            regUser.put("Tel", tel);
+            regUser.put("IC", ic);
+            regUser.put("Password", password);
+            regUser.put("Name", name);
+            regUser.put("StockID", stockistID);
+            regUser.saveInBackground();
+
+            startActivity(new Intent(this, MainActivity.class));
+            Toast.makeText(getApplicationContext(), "Register Successful!!!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
